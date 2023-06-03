db0b5bd2445699d5d91974949433338d52e89d3b
IndianBartonka <bartonka2017@outlook.com>
2023-06-04 01:42:52 +0200

    Implement metrics util for storage plugin id

---
+++ Core/src/main/java/me/indian/safetyproxy/other/MetricsUtil.java
@@ -0,0 +1,8 @@
+package me.indian.safetyproxy.other;
+
+public class MetricsUtil {
+
+    // Metrics currently not support WaterDogPe , we then try to add own fork for it
+    public static int pluginId = 18665;
+
+}
